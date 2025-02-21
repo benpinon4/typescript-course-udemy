17fbb8cce6143700131bfa3341ae5eaa175c59a0
benpinon4 <benjamin.pinon@CodeImmersives.com>
2025-02-20 21:28:32 -0500

    completed some document query selection in basic ts app

--- mini-project/dist/main.js
+++ mini-project/dist/main.js
@@ -1,3 +1,15 @@
 "use strict";
 console.log("Hello Mini Project");
-let header = document.querySelector("h1");
+let header = document.getElementById("parent");
+console.log(header);
+const newDiv = document.createElement("div");
+console.log(newDiv);
+// Set properties (e.g., class, text content)
+newDiv.className = "new-class";
+newDiv.textContent = "Hello, this is a new div!";
+// Select the parent element where you want to append
+const parentElement = document.getElementById("parent");
+// Append the new element to the parent
+if (parentElement) {
+    parentElement.appendChild(newDiv);
+}
